603294432db68f98422a4be6ff85dc58c2dd307f
fazli azim <fazli@knowpakistan.org>
2024-02-03 16:09:42 +0500

    iteration

--- src/component/Bookmarks.jsx
+++ src/component/Bookmarks.jsx
@@ -6,9 +6,9 @@ const Bookmarks = ({title,data}) => {
     <div className='px-20 pt-12'>
       <h1 className='text-xl font-bold   '>{title}</h1>
       <div className='py-5 flex gap-4'>
-        {data?.map((item)=>{
+        {data?.map((item,index)=>{
             return(
-                <div className='ring-1 ring-gray-200 w-[17%] duration-500 transition-all hoverShadow  rounded-md h-[10rem] p-4'>
+                <div key={index} className='ring-1 ring-gray-200 w-[17%] duration-500 transition-all hoverShadow  rounded-md h-[10rem] p-4'>
                 <i className={item.icon} style={{padding:'0.3rem',fontSize:'2rem',color:item?.bgcolor}}></i>
                 <h1 className='text-base font-bold py-2'>{item.title}</h1>
                 <p className='text-xs text-gray-500'>{item.desc}</p>
